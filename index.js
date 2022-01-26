9fe37cb045cf8b796599d812d2bc67e0d62d6681
Amankumar1997 <38433641+Amankumar1997@users.noreply.github.com>
2022-01-26 21:40:52 +0530

    this basic steup of my project is done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+
+const port =8000;
+app.listen(port,(err)=>{
+
+    if(err)
+    {
+        console.log(`there will error on server listen${err}`);
+    }
+    console.log(`server ruuniing on prot no${port}`);
+   
+})
